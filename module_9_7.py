2e56cbe00fb232a486d646941e9e6faf3d2934ff
ПК <@DESKTOP-H9D4HFD>
2024-11-03 22:10:36 +0700

    Добавьте файлы проекта.

---
+++ module_9_7.py
@@ -0,0 +1,23 @@
+﻿def is_prime(funhin):
+    def wrapper(*args, **kwargs):
+        k = 0
+        result = funhin(*args, **kwargs)
+        for i  in range(2, result-1):
+          
+            if result % i == 0:
+                 k = 0
+                 break
+            else: 
+                k = 1
+
+        if k != 0:
+            return f'Простое \n{result}'
+        else:
+            return f'Состовное \n{result}'
+    return wrapper
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+result = sum_three(2, 3, 6)
+print(result)
